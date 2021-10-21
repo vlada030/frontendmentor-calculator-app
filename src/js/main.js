f0ad08af716dedbc1abdd146a63ba7afaa70717e
Vlada FKZ Laptop <vlada030@gmail.com>
2021-10-21 13:33:23 +0200

    postavljen UI sa toggle theme JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-import '../scss/main.scss'
+import "../scss/main.scss";
+
+const themeSelectorBtn = document.getElementById("themeSelectorBtn");
+//const body = document.querySelector("body");
+const body = document.body;
+
+themeSelectorBtn.addEventListener("click", () => {
+    const classArr = ["dark", "light", "custom"];
+
+    const arrLength = classArr.length;
+
+    for (let i = 0; i < arrLength; i++) {
+        if (body.classList.contains(classArr[i])) {
+            body.classList.toggle(classArr[i], false);
+
+            i !== arrLength - 1
+                ? body.classList.toggle(classArr[i + 1], true)
+                : body.classList.toggle(classArr[0], true);
+            return;
+        }
+    }
+});
